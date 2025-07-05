d576a4b4eb517d23b6111ed33e38ece3c496cbb6
manoj12235 <manojsinghtomar134@gmail.com>
2025-07-05 18:08:32 +0530

    added new rg

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ resource "azurerm_resource_group" "manoj" {
   location = "West Europe"
 }
 
+resource "azurerm_resource_group" "manoj1" {
+  name     = "manoj-singh"
+  location = "West Europe"
+}
 
 #manoj singhto marrr rr rr
 #new content add in code 
